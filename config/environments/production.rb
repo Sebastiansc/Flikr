0ca462fc798251c2664b943bf4524a910bca3c9d
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-10-29 21:40:46 -0700

    update production.rb for assets on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.serve_static_files = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -54,7 +56,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "BenchBnB_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Flikr_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.

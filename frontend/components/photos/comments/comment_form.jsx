7c3b4fe76597bbf8bc599c15feb1a0f03324ce14
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-05 20:07:59 -0700

    Basic styling for comment form

--- frontend/components/photos/comments/comment_form.jsx
+++ frontend/components/photos/comments/comment_form.jsx
@@ -3,15 +3,24 @@ import React from 'react';
 class CommentForm extends React.Component{
   constructor(props){
     super(props);
+    this.state = {comment: ''};
   }
 
+  update(e){
+    this.setState({comment: e.target.value});
+  }
 
   render(){
+    const klass = this.state.comment ?
+      'submit-comment' :
+      'submit-comment disable-comments';
+      
     return(
       <div className='comment-form'>
-        <textarea>
-
-        </textarea>
+        <textarea onChange={e => this.update(e)}></textarea>
+        <button className={klass}>
+          Comment
+        </button>
       </div>
     );
   }

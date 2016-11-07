1263a2d7af05ea7ab2454894e4384351b87a409a
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-07 08:40:43 -0800

    Add autoplay for splash main image

--- frontend/components/splash/splash_image.jsx
+++ frontend/components/splash/splash_image.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
-import Slider from 'react-slick';
 
-const settings = {
-  dots: false,
-  infinite: true,
-  speed: 1,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  autoplay: true,
-  lazyLoad: true
-};
+export default class SplashImage extends React.Component{
+  constructor(){
+    super();
+    this.images = [
+      'https://res.cloudinary.com/flikr/image/upload/v1478504249/01-interior-contest-grand-prize-winner_dt4n10.jpg',
+      'https://res.cloudinary.com/flikr/image/upload/v1478504274/i5zceqigrcldqgktf6wj_g5asvq.jpg',
+      'https://res.cloudinary.com/flikr/image/upload/v1478504263/78722_1600x1200-wallpaper-cb1398895960_yvyebw.jpg'
+    ];
+    this.state = {url: this.images[0], current: 0};
+    this.autoplay = window.setInterval(() => this.next(), 4000);
+  }
 
-const SplashImage = () => (
-  <div className='splash-main-image'
-    style={{backgroundImage: `url('https://res.cloudinary.com/flikr/image/upload/v1478504274/i5zceqigrcldqgktf6wj_g5asvq.jpg')`}}>
-    <div className='splash-main-text'>
-      <h1>The home for all your photos</h1>
-      <p>Upload, access, organize and share your photos from anywhere in the world</p>
-      <Link to='signup' className='photo-upload make-it-pop'>Sign Up</Link>
-    </div>
-  </div>
-);
+  next(){
+    const nextIndex = this.state.current === 2 ? 0 : this.state.current + 1;
+    this.setState({url: this.images[nextIndex], current: nextIndex});
+  }
 
-export default SplashImage;
+  render(){
+    return(
+      <div className='splash-main-image'
+        style={{backgroundImage: `url('${this.state.url}')`}}>
+        <div className='splash-main-text'>
+          <h1>The home for all your photos</h1>
+          <p>Upload, access, organize and share your photos from anywhere in the world</p>
+          <Link to='signup' className='photo-upload make-it-pop'>Sign Up</Link>
+        </div>
+      </div>
+    );
+  }
+}

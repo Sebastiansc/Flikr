08cf2909a5f161e74f5f2656ab4e526de3dd0d5e
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-09 16:52:33 -0800

    Replace masonry with justified gallery for main gallery

--- frontend/components/photos/explore.jsx
+++ frontend/components/photos/explore.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
-import ExploreGalleryItem from './explore_gallery_item';
-import Masonry from 'react-masonry-component';
-
-const masonryOptions = {
-  transitionDuration: 0
-};
+import JustifiedGallery from './justified_gallery';
 
 const ExploreGallery = ({photos, klass}) =>(
   <div>
-    <Masonry className={klass} options={masonryOptions}>
-      {photos.map( photo => {
-        return (
-          <ExploreGalleryItem
-            key={photo.id} photo={photo}/>
-        );
-      })}
-    </Masonry>
+    <JustifiedGallery photos={photos}
+        rowHeight={230}/>
   </div>
 );
 
 export default ExploreGallery;
-
-// export default class ExploreGalley extends React.Component{
-//   constructor(){
-//     super();
-//     this.state = ({loaded: })
-//   }
-// }
